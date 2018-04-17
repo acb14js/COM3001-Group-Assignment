61b515c8ea749156d9c460d2ca12054c3c6a60ae
Joseph Mason <Acb14js@shefuniad.shef.ac.uk>
2018-04-17 14:11:48 +0100

    Initial commit

---
+++ runnerloop.m
@@ -0,0 +1,12 @@
+test_values = [50 100 150 200 250 300 350]
+
+for elm = test_values
+    ecolab(40+elm, 80, 30, 100)
+    ecolab(40+elm, 100, 30, 100)
+    ecolab(40+elm, 120, 10, 100)
+    ecolab(40+elm, 120, 20, 100)
+    ecolab(40+elm, 120, 30, 100)
+    ecolab(40+elm, 120, 50, 100)
+    ecolab(40+elm, 120, 70, 100)
+    ecolab(40+elm, 200, 50, 100)
+end
